69ff1990ea760eee56827c2334f57f68b060c316
cekharchandra-devops <cekharchandra.devops@gmail.com>
2025-01-07 13:09:47 +0530

    this repo is for testing terraform aws vpc module

---
+++ main.tf
@@ -0,0 +1,9 @@
+module "vpc" {
+  source = "../09-tf-vpc-module"
+  project_name = "expense"
+  environmet = "dev"
+  public_subnet_cidr_blocks = var.public_subnet_cidr_blocks
+  private_subnet_cidr_blocks = var.private_subnet_cidr_blocks
+  db_subnet_cidr_blocks = var.db_subnet_cidr_blocks
+}
+
---
+++ output.tf
@@ -0,0 +1,7 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+# output "az_available" {
+#   value = module.vpc.availability_zones
+# }
